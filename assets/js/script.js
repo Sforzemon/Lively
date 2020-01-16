84e27b4c1910905f005e321e22818398448e8d05
Sforzemon <sforzemon@gmail.com>
2020-01-16 18:48:17 -0500

    merging Mike and Eric's stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,11 @@
 
 $( document ).ready(function() {
+    
     var artistName = "";
     var savedSearches = [];
-
-    function setArtist(){
-        artistSaved = JSON.parse(localStorage.setItem('#getArtistName'))
-    }
-    function localeGet(){
         if (localStorage.getItem('savedSearches') !== null) {
             savedSearches = JSON.parse(localStorage.getItem('savedSearches'));  
         } 
-    }
-        
     
 
     $(document).on('keypress',function(event) {
@@ -19,46 +13,65 @@ $( document ).ready(function() {
             event.preventDefault();
             artistName = $("#getArtistName").val().trim();
             getBand();
-            setArtist();
-            localeGet();
         }
     });
-
     $("#topNavSearch").on("click", function(event) {
         event.preventDefault();
         artistName = $("#getArtistName").val().trim();
         getBand();
-        getGiphy();
     });
-    // function myFavorites() {
 
-    //     savedSearches.unshift(recentSearch)
+    $("#loveThisBand").on("click", function(e) {
+        e.preventDefault();
+        myFavorites()
+    });
+
+
+    // function myFavorites() {
+    //     var loveTheseGuys = $("#bandName").text
+    //     var favoriteBands = {
+    //         name: loveTheseGuys,
+    //         value: ""
+    //     }
+    //     console.log(loveTheseGuys)
+    //     console.log(favoriteBands)
+    //     savedSearches.unshift(favoriteBands)
     //     localStorage.setItem("savedSearches", JSON.stringify(savedSearches));
+    //     $(".dropdown-header").empty();
+    //     for (i=0; i<savedSearches.length; i++) {
+    //         var newAnchor = $("<a>", {"class": "dropdown-item"})
+    //         $(".dropdown-header").append(newAnchor);
+    //         newAnchor.text(savedSearches[i]);
+    //     }
     // }
 
 
 
     function getBand() {
-        var lastFMURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artistName + "&api_key=7ee5b384da21658ce5fd68901750d490&format=json";        
+        var lastFMURLbio = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artistName + "&api_key=7ee5b384da21658ce5fd68901750d490&format=json";        
+        var lastFMURLTopTracks = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=" + artistName + "&api_key=7ee5b384da21658ce5fd68901750d490&format=json";
+        var lastFMURLTopAlbum = "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=" + artistName + "&api_key=7ee5b384da21658ce5fd68901750d490&format=json";
+        var tasteURL = "https://tastedive.com/api/similar?q=" + artistName + "&k=353205-Lively-TT1A0QYZ&verbose=1"
         $.ajax({
-            url: lastFMURL,
+            url: lastFMURLbio,
             method: "GET"
         }).then(function(response) {
-            var recentSearch = {
-                bandName: response.artist.name,
-                id: response.artist.stats.listeners
+            $("h4").remove();
+            var theBand = $("<h4>", {"class": "ml-2 d-inline-block my-auto"});
+            $("#bandName").append(theBand)
+            theBand.empty()
+            theBand.text(response.artist.name);
+            var bandBioTest = response.artist.bio.content;
+            bandBioTest = bandBioTest.split(', C')[0];
+            console.log(bandBioTest)
+            if (bandBioTest === "WRONG NAME") {
+                bandBioTest = "Sorry, this artist is lacking an up to date Bio."
+                $("#bandBio").text(bandBioTest)
             }
-
-            $("#bandName").text(recentSearch.bandName);
-
-            $("#bandBio").append($("<p>", {id: "bandSummary"}));
-
-            var editedBio = response.artist.bio.slice(0,-68)
-
-            $("#bandSummary").text(editedBio);
-
+            else {
+                $("#bandBio").text(response.artist.bio.content);
+            }
             console.log(response.artist.name);
-<<<<<<< HEAD
             console.log(response.artist.bio.content);
             $.ajax({
                 url: lastFMURLTopTracks,
@@ -91,55 +104,45 @@ $( document ).ready(function() {
                         $("#topAlbum").attr("src", albumImg)
                         console.log(albumImg)
                         // document.querySelector("#kvov2 > span.blockInner > span:nth-child(3) > span.blockInner > span:nth-child(1) > span.s > span > a")
-                                                    $.ajax({
-                                                        url: tasteURL,
-                                                        crossOrigin: true,
-                                                        dataType: "jsonp",
-                                                        method: "GET"
-                                                    }).then(
-                                                        $.ajax({
-                                                            url: tasteURL,
-                                                            crossOrigin: true,
-                                                        dataType: "jsonp",
-                                                            method: "GET"
-                                                        }).then(function(response) {
-                                                            var similarArtistOne = response.Similar.Results[0].Name;
-                                                            var similarArtistTwo = response.Similar.Results[1].Name;
-                                                            var similarArtistThree = response.Similar.Results[2].Name;
-                                                            var mainSearchArtistYURL = response.Similar.Info[0].yUrl;
-                                                               $("#youtube-left").attr("data-theVideo", "mainSearchArtistYURL");
-                                                            console.log(response.Similar)
+                        $.ajax({
+                            url: tasteURL,
+                            crossOrigin: true,
+                            dataType: "jsonp",
+                            method: "GET"
+                        }).then(
+                            $.ajax({
+                                url: tasteURL,
+                                crossOrigin: true,
+                            dataType: "jsonp",
+                                method: "GET"
+                            }).then(function(response) {
+                                var similarArtistOne = response.Similar.Results[0].Name;
+                                var similarArtistTwo = response.Similar.Results[1].Name;
+                                var similarArtistThree = response.Similar.Results[2].Name;
+                                var mainSearchArtistYURL = response.Similar.Info[0].yUrl;
+                                    $("#youtube-left").attr("data-theVideo", "mainSearchArtistYURL");
+                                console.log(response.Similar)
+                                function getGiphy(){
+                                    artistName = $("#getArtistName").val().trim();
+                                    var giphyURL = "http://api.giphy.com/v1/gifs/search?q=" + artistName + "&api_key=UyP7yHq6GHhRpGn3p7vtXmNOnjY2UMXT";
+                                    $.ajax({
+                                        url: giphyURL,
+                                        method: "GET"
+                                    }).then(function(data){
+                                        
+                                        console.log(data);
+                                        
+                                    })
+                                }
 
-                                                        }));
+                            }));
                 
                 });
             });
-=======
-
-            console.log(response.artist.bio.content.slice(0,-68));
-
-            
-            
-
-
-
->>>>>>> 679aeb58008b150eeea722ac0922fce31f61f5b4
         });
-    };
+    });
+};
 });
-
-function getGiphy(){
-    artistName = $("#getArtistName").val().trim();
-    var giphyURL = "http://api.giphy.com/v1/gifs/search?q=" + artistName + "&api_key=UyP7yHq6GHhRpGn3p7vtXmNOnjY2UMXT";
-    $.ajax({
-        url: giphyURL,
-        method: "GET"
-    }).then(function(data){
-       
-        console.log(data);
-        
-    })
-}
 
 autoPlayYouTubeModal();
 
