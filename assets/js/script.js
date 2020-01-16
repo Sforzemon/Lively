679aeb58008b150eeea722ac0922fce31f61f5b4
ZVLU <zvluwork@gmail.com>
2020-01-16 18:29:32 -0500

    Second Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
 
 $( document ).ready(function() {
-    
     var artistName = "";
     var savedSearches = [];
+
+    function setArtist(){
+        artistSaved = JSON.parse(localStorage.setItem('#getArtistName'))
+    }
+    function localeGet(){
         if (localStorage.getItem('savedSearches') !== null) {
             savedSearches = JSON.parse(localStorage.getItem('savedSearches'));  
         } 
+    }
+        
     
 
     $(document).on('keypress',function(event) {
@@ -13,123 +19,69 @@ $( document ).ready(function() {
             event.preventDefault();
             artistName = $("#getArtistName").val().trim();
             getBand();
+            setArtist();
+            localeGet();
         }
     });
+
     $("#topNavSearch").on("click", function(event) {
         event.preventDefault();
         artistName = $("#getArtistName").val().trim();
         getBand();
+        getGiphy();
     });
-
-    $("#loveThisBand").on("click", function(e) {
-        e.preventDefault();
-        myFavorites()
-    });
-
-
     // function myFavorites() {
-    //     var loveTheseGuys = $("#bandName").text
-    //     var favoriteBands = {
-    //         name: loveTheseGuys,
-    //         value: ""
-    //     }
-    //     console.log(loveTheseGuys)
-    //     console.log(favoriteBands)
-    //     savedSearches.unshift(favoriteBands)
+
+    //     savedSearches.unshift(recentSearch)
     //     localStorage.setItem("savedSearches", JSON.stringify(savedSearches));
-    //     $(".dropdown-header").empty();
-    //     for (i=0; i<savedSearches.length; i++) {
-    //         var newAnchor = $("<a>", {"class": "dropdown-item"})
-    //         $(".dropdown-header").append(newAnchor);
-    //         newAnchor.text(savedSearches[i]);
-    //     }
     // }
 
 
 
     function getBand() {
-        var lastFMURLbio = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artistName + "&api_key=7ee5b384da21658ce5fd68901750d490&format=json";        
-        var lastFMURLTopTracks = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=" + artistName + "&api_key=7ee5b384da21658ce5fd68901750d490&format=json";
-        var lastFMURLTopAlbum = "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=" + artistName + "&api_key=7ee5b384da21658ce5fd68901750d490&format=json";
-        var tasteURL = "https://tastedive.com/api/similar?q=" + artistName + "&k=353205-Lively-TT1A0QYZ&verbose=1"
+        var lastFMURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artistName + "&api_key=7ee5b384da21658ce5fd68901750d490&format=json";        
         $.ajax({
-            url: lastFMURLbio,
+            url: lastFMURL,
             method: "GET"
         }).then(function(response) {
-            $("h4").remove();
-            var theBand = $("<h4>", {"class": "ml-2 d-inline-block my-auto"});
-            $("#bandName").append(theBand)
-            theBand.empty()
-            theBand.text(response.artist.name);
-            var bandBioTest = response.artist.bio.content;
-            bandBioTest = bandBioTest.split(', C')[0];
-            console.log(bandBioTest)
-            if (bandBioTest === "WRONG NAME") {
-                bandBioTest = "Sorry, this artist is lacking an up to date Bio."
-                $("#bandBio").text(bandBioTest)
+            var recentSearch = {
+                bandName: response.artist.name,
+                id: response.artist.stats.listeners
             }
-            else {
-                $("#bandBio").text(response.artist.bio.content);
-            }
-            console.log(response.artist.name);
-            console.log(response.artist.bio.content);
-            $.ajax({
-                url: lastFMURLTopTracks,
-                method: "GET"
-            }).then(function(response) {
-                var trackGold = response.toptracks.track[0].name;
-                var scrobblesOne = response.toptracks.track[0].playcount;
-                var trackSilver = response.toptracks.track[1].name;
-                var scrobblesTwo = response.toptracks.track[1].playcount;
-                var trackBronze = response.toptracks.track[2].name;
-                var scrobblesThree = response.toptracks.track[2].playcount;
-                $(".oneTrack").text(trackGold);
-                $(".oneScrobble").text (scrobblesOne + " times!");
-                $(".twoTrack").text(trackSilver);
-                $(".twoScrobble").text (scrobblesTwo + " times!");
-                $(".threeTrack").text(trackBronze);
-                $(".threeScrobble").text (scrobblesThree + " times!");
-                $.ajax({
-                    url: lastFMURLTopAlbum,
-                    method: "GET"
-                }).then(function(response) {
-                    var albumName = response.topalbums.album[0].name;
-                    var lastFMURLAlbumInfo = "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&artist=" + artistName + "&album=" + albumName + "&api_key=7ee5b384da21658ce5fd68901750d490&format=json"
-                    console.log(response.topalbums.album[0].name)
-                    $.ajax({
-                        url: lastFMURLAlbumInfo,
-                        method: "GET"
-                    }).then(function(response) {
-                        var albumImg = response.album.image[2]["#text"];
-                        $("#topAlbum").attr("src", albumImg)
-                        console.log(albumImg)
-                        // document.querySelector("#kvov2 > span.blockInner > span:nth-child(3) > span.blockInner > span:nth-child(1) > span.s > span > a")
-                                                    // $.ajax({
-                                                    //     url: tasteURL,
-                                                    //     headers: {
-                                                    //         "Access-Control-Allow-Origin": '*' 
-                                                    //     },
-                                                    //     method: "GET"
-                                                    // }).then(
-                                                    //     $.ajax({
-                                                    //         url: tasteURL,
-                                                    //         method: "GET"
-                                                    //     }).then(function(response) {
-                                                    //         var similarArtistOne = response.Similar.Results[0].Name;
-                                                    //         var similarArtistTwo = response.Similar.Results[1].Name;
-                                                    //         var similarArtistThree = response.Similar.Results[2].Name;
-                                                    //         var mainSearchArtistYURL = response.Similar.Info[0].yUrl;
-                                                    //            $("#youtube-left").attr("data-theVideo", "mainSearchArtistYURL");
-                                                    //         console.log(response.Similar)
 
-                                                    //     }));
-                
-                });
-            });
+            $("#bandName").text(recentSearch.bandName);
+
+            $("#bandBio").append($("<p>", {id: "bandSummary"}));
+
+            var editedBio = response.artist.bio.slice(0,-68)
+
+            $("#bandSummary").text(editedBio);
+
+            console.log(response.artist.name);
+
+            console.log(response.artist.bio.content.slice(0,-68));
+
+            
+            
+
+
+
         });
-    });
-};
+    };
 });
+
+function getGiphy(){
+    artistName = $("#getArtistName").val().trim();
+    var giphyURL = "http://api.giphy.com/v1/gifs/search?q=" + artistName + "&api_key=UyP7yHq6GHhRpGn3p7vtXmNOnjY2UMXT";
+    $.ajax({
+        url: giphyURL,
+        method: "GET"
+    }).then(function(data){
+       
+        console.log(data);
+        
+    })
+}
 
 autoPlayYouTubeModal();
 
